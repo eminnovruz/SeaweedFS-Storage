5b3070d6490b8930397c5dab448bc4b9447cd2d8
novru <novru@EMINSIDEAPAD>
2024-08-01 14:19:21 +0400

    Checking Statement for existing assign with given secret name.

---
+++ FileServer-Asp/CustomExceptions/ExistingAssignException.cs
@@ -0,0 +1,8 @@
+﻿namespace FileServer_Asp.CustomExceptions;
+
+public class ExistingAssignException : Exception
+{
+    public ExistingAssignException(string message) : base(message)
+    {
+    }
+}
--- FileServer-Asp/Services/FileRegisterService.cs
+++ FileServer-Asp/Services/FileRegisterService.cs
@@ -1,4 +1,5 @@
-﻿using FileServer_Asp.Data;
+﻿using FileServer_Asp.CustomExceptions;
+using FileServer_Asp.Data;
 using FileServer_Asp.Entities;
 using FileServer_Asp.JsonModels;
 using FileServer_Asp.Models;
@@ -18,13 +19,25 @@ public class FileRegisterService : IFileRegisterService
 
     public async Task RegisterFile(FileModel fileToUpload, string fid)
     {
-        await _context.Assigns.InsertOneAsync(new AssignEntity()
+        FilterDefinition<AssignEntity> filter = Builders<AssignEntity>.Filter.Eq(a => a.SecretName, fileToUpload.SecretName);
+
+        AssignEntity existingEntity = await _context.Assigns.Find(filter).FirstOrDefaultAsync();
+
+        if (existingEntity != null)
+        {
+            throw new ExistingAssignException("There is another assign with given secret name");
+        }
+
+        var newEntity = new AssignEntity
         {
             Id = Guid.NewGuid().ToString(),
             SecretName = fileToUpload.SecretName,
             Fid = fid
-        });
+        };
+
+        await _context.Assigns.InsertOneAsync(newEntity);
     }
+
 
     public async Task<AssignEntity> ViewFileViaSecretName(string secretName)
     {
